3c3da18413975722fe48ee7d2f405a9622bca617
PremChand34555 <premthakurfact786@gmail.com>
2025-06-23 18:01:18 +0500

    new update

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom'; // ✅ import HashRouter
+import { HashRouter } from 'react-router-dom'; // ✅ Add this
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <HashRouter> {/* ✅ wrap App in HashRouter */}
+    <HashRouter> {/* ✅ Wrap App */}
       <App />
     </HashRouter>
   </React.StrictMode>
